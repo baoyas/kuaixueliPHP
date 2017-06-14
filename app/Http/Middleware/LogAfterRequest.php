06f64d09f5f9ee4624dfcca2bc3ab019b12006c7
Gogs <gogs@fake.local>
2017-06-14 18:53:32 +0800

    fix

---
+++ app/Http/Middleware/LogAfterRequest.php
@@ -0,0 +1,52 @@
+<?php
+namespace App\Http\Middleware;
+use DB;
+use Closure;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Formatter\LineFormatter;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
+class LogAfterRequest {
+
+    public function handle(Request $request, Closure $next)
+    {
+        DB::enableQueryLog();
+        return $next($request);
+    }
+
+    public function terminate(Request $request, $response)
+    {
+        $formatter = new LineFormatter(null, null, true, true);
+        $log = new Logger('access');
+        $log->pushHandler((new StreamHandler(storage_path().'/logs/access_'.date('Y-m-d').'.log', Logger::INFO))->setFormatter($formatter) );
+        $jsonHeader = json_encode($request->headers->all(), JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+        $jsonRequest = print_r($request->all(), true);
+        $logFun = 'info';
+        if(stripos($response->headers->get('Content-Type'), 'image')!==false) {
+            $jsonResponse = 'content-type: '.$response->headers->get('Content-Type');
+        } else if($response instanceof JsonResponse) {
+            $jsonResponse = json_encode($response->getData(), JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+            if($response->getData()->code!==0) {
+                $logFun = 'error';
+            }
+        } else if($response instanceof Response) {
+            $jsonResponse = $response->getContent();
+        }
+        $sqlArr = [];
+        $queries = DB::getQueryLog();
+        foreach ($queries as $k=>$query) {
+            $sql = $query['query'];       //查询语句sql
+            $params = $query['bindings']; //查询参数
+            $sql = str_replace('?', "'%s'", $sql);
+            array_unshift($params, $sql);
+            $sqlArr[] = call_user_func_array('sprintf', $params)."; {$query['time']}";
+            unset($queries[$k]['bindings'], $queries[$k]['time']);
+        }
+        $jsonSql = print_r($sqlArr, true);
+        app('log')->$logFun('fullurl==>'.$request->fullUrl()."\nhttpmethod==>".$request->getMethod()."\nheader==>{$jsonHeader}\nrequest==>{$jsonRequest}\nresponse==>{$jsonResponse}\nsql==>{$jsonSql}");
+    }
+
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,10 +10,41 @@
 | the IoC container for the system binding all of the various parts.
 |
 */
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Formatter\LineFormatter;
+
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 
 $app = new Illuminate\Foundation\Application(
     realpath(__DIR__.'/../')
 );
+
+
+//域名配置
+$env_hosts = [
+    'test' => ['127.0.0.1'],
+    'prod' => ['api.ldlchat.com', 'www.ldlchat.com', 'ldlchat.com']
+];
+//环境处理
+if($app->runningInConsole()) {//命令行
+    $app_env = 'test';
+} else {//HTTP形式
+    $http_host = str_replace(':' . $_SERVER['SERVER_PORT'], '', $_SERVER['HTTP_HOST']);
+    if (empty($http_host)) {
+        die('[error] no host');
+    }
+    foreach ($env_hosts as $env => $hosts) {
+        if (in_array($http_host, $hosts)) {
+            $app_env = $env;
+            break;
+        }
+    }
+}
+if (empty($app_env) || empty($env_hosts[$app_env])) {
+    die('[error] no environment');
+}
+$app->loadEnvironmentFrom('env.'.$app_env);
 
 /*
 |--------------------------------------------------------------------------
@@ -40,6 +71,46 @@ $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
     App\Exceptions\Handler::class
 );
+
+
+
+
+
+
+
+$app->configureMonologUsing(function($monolog) {
+    $formatter = new LineFormatter(null, null, true, true);
+    class LogFileHandler extends StreamHandler
+    {
+        public function __construct($stream, $level, $bubble, $filePermission, $useLocking)
+        {
+            parent::__construct($stream, $level, $bubble, $filePermission, $useLocking);
+        }
+
+        public function handleBatch(array $records)
+        {
+            $log = "";
+            foreach ($records as $record) {
+                if (!$this->isHandling($record)) {
+                    continue;
+                }
+                $record = $this->processRecord($record);
+                $log .= $this->getFormatter()->format($record);
+            }
+            // 调用日志写入方法
+            $this->write(['formatted' => $log]);
+        }
+    }
+    $LogFileHandler = new LogFileHandler(storage_path().'/logs/'.date('Y-m-d').'.log');
+    $LogFileHandler->setFormatter($formatter);
+    $monolog->pushHandler(new \Monolog\Handler\BufferHandler($LogFileHandler));
+});
+
+
+
+
+
+
 
 /*
 |--------------------------------------------------------------------------

e2be103989eae706231aa5576fcb54aff733766e
Gogs <gogs@fake.local>
2017-06-19 16:44:38 +0800

    laravel-admin

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -44,6 +44,7 @@ return array(
     'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
     'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
     'Faker\\' => array($vendorDir . '/fzaninotto/faker/src/Faker'),
+    'Encore\\Admin\\' => array($vendorDir . '/encore/laravel-admin/src'),
     'Dotenv\\' => array($vendorDir . '/vlucas/phpdotenv/src'),
     'Doctrine\\Instantiator\\' => array($vendorDir . '/doctrine/instantiator/src/Doctrine/Instantiator'),
     'Dingo\\Blueprint\\' => array($vendorDir . '/dingo/blueprint/src'),

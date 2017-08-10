0d503b8aff7a86d2522d58604129267196c06a2d
Gogs <gogs@fake.local>
2017-08-10 10:27:34 +0800

    fix

---
+++ app/Model/EducationOrder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EducationOrder extends Model
+{
+    protected $table='education_order';
+    protected $primaryKey='id';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'education_id',
+        'order_no',
+        'status',
+        'name',
+        'level_1_id',
+        'level_2_id',
+        'level_3_id',
+        'school_id',
+        'studymode_id',
+        'major',
+        'fulltime_id',
+        'notfulltime_id',
+        'length',
+        'province_desc',
+        'coaches',
+        'admission',
+        'entry_fee',
+        'market_fee',
+        'kxl_fee',
+    ];
+
+    public $timestamps=true;
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+}

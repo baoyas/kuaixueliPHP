7b0dc19d229378c953b168ace0a7e0b9f6457e9e
Gogs <gogs@fake.local>
2017-08-14 14:46:10 +0800

    fix

--- app/Http/Controllers/Home/EducationController.php
+++ app/Http/Controllers/Home/EducationController.php
@@ -13,17 +13,17 @@ class EducationController extends Controller
     {
         $level_id = $request->get('level_id');
         $province_id = $request->get('province_id', 0);
-        $edu = Education::with('school')->with('provinces')->with('contacts')->where('level_1_id', $level_id)->orWhere('level_2_id', $level_id)->orWhere('level_3_id', $level_id)->get();
+        $edu = Education::with('school.province')->with('provinces')->with('contacts')->where('level_1_id', $level_id)->orWhere('level_2_id', $level_id)->orWhere('level_3_id', $level_id)->get();
         $eLevel = EducationLevel::find($level_id);
         $provinces = [];
         foreach($edu as $e) {
-            foreach($e->provinces as $p) {
-                $provinces[$p->id] = $p->name;
+            if($e->school->province) {
+                $provinces[$e->school->province->id] = $e->school->province->name;
             }
         }
         if(!empty($province_id)) {
-            $edu = Education::with('school')->with(['provinces'=>function($query) use($province_id) {
-                $query->where('province_id', $province_id);
+            $edu = Education::with(['school.province'=>function($query) use($province_id) {
+                $query->where('id', $province_id);
             }])->with('contacts')->where('level_1_id', $level_id)->orWhere('level_2_id', $level_id)->orWhere('level_3_id', $level_id)->get();
 
         }
